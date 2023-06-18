7f1d80a89e84e78d35b39fd14ff5dc2b62ddbf90
Simon Ainslie <srainslie@protonmail.com>
2023-06-18 18:31:50 +0100

    Refactoring scorecard for frontend.

--- Entities/Scorecard.cs
+++ Entities/Scorecard.cs
@@ -5,19 +5,19 @@ namespace WebApi.Entities
        public Scorecard(Course course, Score score, List<Tuple<int, int>> scores, List<Tuple<int, int>> pars){
 
            courseName = course.Name;
-           roundDate = score.date;
+           roundDate = score.date.Date;
            front9_total = score.front9_total;
            back9_total = score.back9_total;
            total = score.total;
            front9ToPar = front9_total - course.front9_par;
            back9ToPar = back9_total - course.back9_par;
            totalToPar = total - course.total;
-           results = new List<hole>();
+           holeResults = new List<hole>();
            for (int i = 0; i < pars.Count; i++)
             {
                 if (scores[i].Item1 == pars[i].Item1)
                 {
-                    results.Add(new hole(scores[i].Item1, scores[i].Item2, pars[i].Item2));    
+                    holeResults.Add(new hole(scores[i].Item1, scores[i].Item2, pars[i].Item2));    
                     
                 } else {
                     throw new Exception();
@@ -27,9 +27,7 @@ namespace WebApi.Entities
 
         public string courseName { get; set; }
         public DateTime roundDate { get; set;}
-
-        public List<hole> results {get; set;}
-
+        public List<hole> holeResults {get; set;}
         public int front9_total { get; set; } 
         public int back9_total { get; set; }
         public int total { get; set; }
